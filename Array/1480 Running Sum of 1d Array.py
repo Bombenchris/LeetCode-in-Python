c4705a384a935ebaf4da13e8ae8ddf669a65d338
Cheng Li <lichengchris@gmail.com>
2020-06-16 22:39:02 +0200

    Update 1480 Running Sum of 1d Array.py
    added performance information

--- Array/1480 Running Sum of 1d Array.py
+++ Array/1480 Running Sum of 1d Array.py
@@ -7,3 +7,6 @@ class Solution:
         for i in range(1, len(nums)):
             runningSum[i] = runningSum[i] + runningSum[i - 1]
         return runningSum
+
+# Runtime: 32 ms, faster than 99.26% of Python3 online submissions for Running Sum of 1d Array.
+# Memory Usage: 13.9 MB, less than 100.00% of Python3 online submissions for Running Sum of 1d Array.
